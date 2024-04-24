ad0420ac40227b2bd21ab80a5e5e9070921097b0
Marcos Antônio <marcosjunior.me.009@gmail.com>
2024-04-24 14:39:20 -0300

    feat: organizando primeiros arquivos de aula introdutória

---
+++ primeiro-projeto/src/edu/marcos/primeirasemana/App.java
@@ -0,0 +1,22 @@
+package edu.marcos.primeirasemana;
+
+public class App {
+    
+    
+    public static String nomeCompleto (String nome1, String nome2) {
+        return "\nResultado da concatenação: " + nome1.concat(" ").concat(nome2);
+    }
+    
+    public static void main(String[] args) {
+        String nomePrimeiro = "Marcos Antônio";
+        String nomeSegundo = "da Silva Junior";
+
+        String nomeTodo = nomeCompleto(nomeSegundo, nomePrimeiro);
+
+        System.out.println(nomeTodo);
+    }
+    
+    
+    
+
+}
---
+++ primeiro-projeto/src/edu/marcos/segundasemana/App.java
@@ -0,0 +1,8 @@
+package edu.marcos.segundasemana;
+
+public class App {
+    
+    public static void main(String[] args) {
+        System.out.println("Estou na pasta segundasemana.\noi\n");
+    }
+}

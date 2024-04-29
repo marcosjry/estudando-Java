d752ee5d2fe222b6a307adcfdfbb186d5e34a573
Marcos Antônio <marcosjunior.me.009@gmail.com>
2024-04-29 19:54:22 -0300

    feat: Exercício de Ordenação Set finalizado

---
+++ collections-java-api/src/set/Ordenacao/CadastroProduto.java
@@ -0,0 +1,51 @@
+package set.Ordenacao;
+
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
+
+public class CadastroProduto {
+    
+    Set<Produto> produtoSet;
+
+    public CadastroProduto() {
+        this.produtoSet = new TreeSet<>();
+    }
+
+    public void adicionarProduto(String nome, long codigo, double preco, int quantidade) {
+        produtoSet.add(new Produto(nome, codigo, preco, quantidade));
+    }
+
+    public Set<Produto> exibirProdutosPorPreco() {
+        //Para ter um conjunto SET ordenado é preciso passar o COMPARATOR no construtor do conjunto 
+        Set<Produto> setPorPreco = new TreeSet<>(Comparator.comparing(Produto::getPreco)); 
+        for(Produto p: produtoSet) {
+            setPorPreco.add(p);
+        }
+        return setPorPreco;
+    }
+
+    public Set<Produto> exibirProdutosPorNome() {
+        //Para ter um conjunto SET ordenado é preciso passar o COMPARATOR no construtor do conjunto 
+        Set<Produto> setPorNome = new TreeSet<>(Comparator.comparing(Produto::getNome)); 
+        for(Produto p: produtoSet) {
+            setPorNome.add(p);
+        }
+        return setPorNome;
+    }
+
+    public static void main(String[] args) {
+        
+        CadastroProduto produtos = new CadastroProduto();
+
+        produtos.adicionarProduto("Cadeira Gamer Pichau", 23564872, 1500.0, 2);
+        produtos.adicionarProduto("Cadeira Ergonômica", 55421235, 2500.10, 3);
+        produtos.adicionarProduto("Monitor AOC 140Hz", 333320121, 1300.0, 1);
+        produtos.adicionarProduto("Teclado Gamer SPACEX", 12456385, 358.50, 4);
+
+        System.out.println(produtos.exibirProdutosPorNome());
+
+        System.out.println(produtos.exibirProdutosPorPreco());
+    }
+}
---
+++ collections-java-api/src/set/Ordenacao/Produto.java
@@ -0,0 +1,48 @@
+package set.Ordenacao;
+
+
+public class Produto implements Comparable<Produto> {
+    private String nome;
+    private long codigo;
+    private double preco;
+    private int quantidade;
+
+    public Produto(String nome, long codigo, double preco, int quantidade) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    @Override
+    public int compareTo(Produto outro) {
+
+        return this.nome.compareTo(outro.getNome());
+    }
+
+    public int precoCrescente(Produto outro) {
+        return Double.compare(this.preco, outro.getPreco());
+    }
+
+    @Override
+    public String toString() {
+        return "\nProduto: "+nome+" Codigo: "+codigo+"\nPreço: "+preco+" Quantidade: "+quantidade;
+    }
+
+}

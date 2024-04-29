e925fc4235c6f5f6c6f07c607d062f176e39e368
Marcos Antônio <marcosjunior.me.009@gmail.com>
2024-04-29 17:21:17 -0300

    feat: exercicio resolvido

---
+++ collections-java-api/src/set/OperacoesBasicas/Palavra.java
@@ -0,0 +1,35 @@
+package set.OperacoesBasicas;
+
+import java.util.Objects;
+
+public class Palavra {
+    
+    private String palavra;
+
+    public Palavra(String palavra){
+        this.palavra = palavra;
+    }
+
+    public String getPalavra() {
+        return palavra;
+    }
+    
+    //Cógido para não inserir mais de um elemento igual.
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) return true;
+        if(!(obj instanceof Palavra palavra)) return false;
+        return getPalavra() == palavra.getPalavra();
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(getPalavra());
+    }
+
+
+    @Override
+    public String toString() {
+        return palavra;
+    }
+}
